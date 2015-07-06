523f304200f33c3e0df20f9407466790ba336fa1
Daniel <dan_pc@live.com>
2015-07-06 11:43:13 -0400

    Add GameCanvas and canvas container

---
+++ DCXPChallengeProject2/app/src/main/java/com/dpc/dcxpchallengeproject2/GameCanvas.java
@@ -0,0 +1,20 @@
+package com.dpc.dcxpchallengeproject2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by Daniel on 7/6/2015.
+ */
+public class GameCanvas extends View {
+
+    public GameCanvas(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
